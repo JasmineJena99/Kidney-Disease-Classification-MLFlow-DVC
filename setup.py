b1ed3ba4a3575f8b97ac1549004906acb4a7137a
Jasmine Jena <roma.jas14@gmail.com>
2023-12-15 18:02:44 +0545

    requirements added

---
+++ setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Kidney-Disease-Classification-MLFlow-DVC"
+AUTHOR_USER_NAME = "JasmineJena99"
+SRC_REPO = "cnnClassifier"
+AUTHOR_EMAIL = "jasminejena19@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for CNN app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
+
+#now we need to create virtual environment
+#open terminal
+#type : conda create -n kidney python=3.8 -y
+#conda activate kidney
+#pip install -r requirements.txt
